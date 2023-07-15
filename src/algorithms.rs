9c97ac4a741f23c55c60953dfec826c29165ee0c
Alex McKenzie <alex.thewlis.mckenzie@gmail.com>
2023-07-15 16:27:21 +0200

    Write outlines of pathfinding algos

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,39 +1,45 @@
-// Some simple algos to try implement:
 
-// Path finding algos
-//  depth-first
-//  breadth-first
-//  A*
 
-// Graph generation (generating all graphs up to certain size)
 
 use crate::representations::UndirectedGraph;
 
-pub fn generate_graphs_up_to(n: usize) -> Vec<UndirectedGraph> {
-    let mut graphs: Vec<UndirectedGraph> = Vec::new();
+mod generate {
+    pub fn generate_graphs_up_to(n: usize) -> Vec<UndirectedGraph> {
+        let mut graphs: Vec<UndirectedGraph> = Vec::new();
 
-    for n_verts in (1..).take(n) {
-        for graph in generate_graphs(n_verts).into_iter() {
-            graphs.push(graph);
+        for n_verts in (1..).take(n) {
+            for graph in generate_graphs(n_verts).into_iter() {
+                graphs.push(graph);
+            }
         }
+        graphs
     }
-    graphs
-}
 
-pub fn generate_graphs(n: usize) -> Vec<UndirectedGraph> {
-    let mut graphs: Vec<UndirectedGraph> = Vec::new();
+    pub fn generate_graphs(n: usize) -> Vec<UndirectedGraph> {
+        let mut graphs: Vec<UndirectedGraph> = Vec::new();
 
-    graphs.push(UndirectedGraph { n_verts: n, edges: Vec::new() });
+        graphs.push(UndirectedGraph { n_verts: n, edges: Vec::new() });
 
-    for i in (0..).take(n) {
-        for j in (0..).take(n) {
-            if i < j {
-                for graph in graphs.clone() {
-                    graphs.push(graph.add_edge(i, j));
+        for i in (0..).take(n) {
+            for j in (0..).take(n) {
+                if i < j {
+                    for graph in graphs.clone() {
+                        graphs.push(graph.add_edge(i, j));
+                    }
                 }
             }
         }
+
+        graphs
+    }
+}
+
+mod pathfinding {
+    def breadth_first_search(graph: &UndirectedGraph, start: usize, end: usize) -> Option<Vec<UndirectedEdge>> {
+        Some(Vec::new())
     }
 
-    graphs
+    def depth_first_search(graph: &UndirectedGraph, start: usize, end: usize) -> Option<Vec<UndirectedEdge>> {
+        Some(Vec::new())
+    }
 }
