bd4cdb62d78a2da0242c4bf62294927f0b3b9aa7
Alex McKenzie <alex.thewlis.mckenzie@gmail.com>
2023-07-20 18:58:14 +0200

    Add test

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,29 @@ use crate::algorithms::generate::generate_graphs_up_to;
 use crate::representations::Graph;
 
 fn main() {
-    // let graph = representations::UndirectedGraph::new()
-    //     .add_edge(0, 1)
-    //     .add_edge(0, 2)
-    //     .add_edge(0, 3)
-    //     .add_edge(0, 4);
-
-    // println!("{:?}", graph);
-    // println!("{:?}", graph.adjacency_matrix());
-
     let graphs = generate_graphs_up_to(4);
     for graph in graphs.into_iter() {
         println!("{:?}", graph.adjacency_matrix());
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn generate_graphs_up_to_3_works() {
+        let graphs = generate_graphs_up_to(3);
+        let expected_graphs = vec![
+            UndirectedGraph::new(),
+            UndirectedGraph { n_verts: 1, edges: Vec.new()},
+            UndirectedGraph { n_verts: 2, edges: Vec.new() },
+            UndirectedGraph { n_verts: 2, edges: Vec.new() }.add_edge(0, 1),
+
+            UndirectedGraph { n_verts: 3, edges: Vec.new() },
+            UndirectedGraph { n_verts: 3, edges: Vec.new() }.add_edge(0, 1),
+            UndirectedGraph { n_verts: 3, edges: Vec.new() }.add_edge(0, 1).add_edge(0, 2),
+            UndirectedGraph { n_verts: 3, edges: Vec.new() }.add_edge(0, 1).add_edge(0, 2).add_edge(1, 2)
+        ]
+        assert_eq!(graphs, expected_graphs);
     }
 }
