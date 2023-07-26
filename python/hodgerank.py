0b64a14435e2cd924234285ff970491495fe4a95
Alex McKenzie <alex.thewlis.mckenzie@gmail.com>
2023-07-26 11:18:49 +0200

    Replace hodgerank with numpy implementation

--- python/hodgerank.py
+++ python/hodgerank.py
@@ -1,80 +1,75 @@
-"""From http://niplav.site/notes.html#Nothing_to_See_Here_Just_An_Implementation_of_HodgeRank"""
+"""
+Alternative hodgerank implementation, bypassing networkx.
 
-from typing import Mapping, TypeVar
+Based on http://niplav.site/notes.html#Nothing_to_See_Here_Just_An_Implementation_of_HodgeRank
+"""
+from typing import Collection, Mapping, TypeVar
+import warnings
 import numpy as np
-import networkx as nx
-import itertools as it
 
 K = TypeVar("K")
 
-
-def positive_edges(prefs):
-    for a, b in it.combinations(prefs.keys(), 2):
-        pref_delta = prefs[a] - prefs[b]
-        if np.all(np.isnan(pref_delta)):
-            continue
-
-        weight = np.nanmean(pref_delta)
-        n = np.sum(~np.isnan(pref_delta))
-        # Add edge in direction where pref_delta is positive
-        if weight >= 0:
-            yield (a, b, {"weight": weight, "n": n})
-        else:
-            yield (b, a, {"weight": -weight, "n": n})
+warnings.simplefilter("ignore", category=RuntimeWarning)
 
 
-def prefgraph(prefs: Mapping[K, np.ndarray[int]]) -> nx.DiGraph:
-    """
-    Construct a directed graph from a set of preferences from voters.
+def prefgraph(
+    prefs: np.ndarray[int, int]
+) -> tuple[np.ndarray[int, int], np.ndarray[int, int]]:
+    """Collapse voter preferences to single preference graph as an adjacency matrix."""
 
-    prefs is a mapping from options to preference vectors. Each preference vector
-    is a numpy array of integers, where the i-th entry is the preference of the
-    i-th voter for that option. Higher numbers mean higher preference.
-    """
-    g = nx.DiGraph()
-    g.add_nodes_from(list(prefs.keys()))
-    edges = positive_edges(prefs)
-    g.add_edges_from(edges)
-    return g
+    prefs_delta = prefs[:, None, :] - prefs[None, :, :]
+
+    counts = np.sum(~np.isnan(prefs_delta), axis=2)
+
+    weights = np.nanmean(prefs_delta, axis=2)
+    weights[weights < 0] = 0
+
+    return weights, counts
 
 
-def decompose(g):
-    f = np.array([g[a][b]["weight"] for a, b in g.edges])
-    W = np.diag([g[a][b]["n"] for a, b in g.edges])
+def decompose(
+    weights: np.ndarray[int, int], counts: np.ndarray[int, int]
+) -> np.ndarray[float]:
+    """Apply the HodgeRank decomposition to obtain a ranking."""
+    n_edges = np.count_nonzero(counts)
+    n_nodes = counts.shape[0]
 
-    origins = np.zeros((g.number_of_edges(), g.number_of_nodes()))
+    nonzero_indices = counts.flatten().nonzero()[0]
+    f = weights.flatten()[nonzero_indices]
+    W = np.diag(counts.flatten()[nonzero_indices])
 
-    idx = {n: i for i, n in enumerate(g.nodes)}
+    signs = np.sign(weights).flatten()[nonzero_indices]
+    starts, ends = counts.nonzero()
 
-    for c, (a, b) in enumerate(g.edges):
-        sign = np.sign(g[a][b]["weight"])
-        if np.isnan(sign):
-            sign = 0
-        origins[c][idx[a]] = -sign
-        origins[c][idx[b]] = sign
+    origins = np.zeros((n_edges, n_nodes))
+    origins[np.arange(n_edges), starts] = -signs
+    origins[np.arange(n_edges), ends] = signs
 
     try:
         s = -np.linalg.pinv(origins.T @ W @ origins) @ origins.T @ W @ f
     except np.linalg.LinAlgError:
-        s = np.zeros(len(list(g.nodes)))
+        s = np.zeros(n_nodes)
 
-    return {n: s[idx[n]] for n in g.nodes}
+    return s
+
+
+def pref_array(
+    prefs: Mapping[K, Collection[int]]
+) -> tuple[Mapping[K, int], np.ndarray[int, int]]:
+    """Convert a preference mapping to a numpy array."""
+
+    num_voters = len(prefs[list(prefs.keys())[0]])
+    num_options = len(prefs)
+    idx = {n: i for i, n in enumerate(prefs.keys())}
+    _prefs = np.zeros((num_options, num_voters))
+    for k, v in prefs.items():
+        _prefs[idx[k], :] = list(v)
+
+    return idx, _prefs
 
 
 def hodgerank(prefs: Mapping[K, np.ndarray[int]]) -> Mapping[K, float]:
-    g = prefgraph(prefs)
-    return decompose(g)
-
-
-def main():
-    prefs = {
-        0: np.array([5, 3]),
-        1: np.array([4, 5]),
-        2: np.array([3, np.nan]),
-        3: np.array([5, 2]),
-    }
-    print(hodgerank(prefs))
-
-
-if __name__ == "__main__":
-    main()
+    idx, _prefs = pref_array(prefs)
+    weights, counts = prefgraph(_prefs)
+    hodge_prefs = decompose(weights, counts)
+    return {k: hodge_prefs[idx[k]] for k in prefs.keys()}
